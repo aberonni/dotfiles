7c4f13cb8563e64603bb2ad1e1e59a83165aef46
Domenico Gemoli <domenicogemoli@gmail.com>
2018-01-18 18:26:17 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,39 @@ RESOURCES_DIRECTORY="${DOTFILES_DIRECTORY}/resources"
 DOTFILES_TARBALL_PATH="https://github.com/aberonni/dotfiles/tarball/master"
 DOTFILES_GIT_REMOTE="https://github.com/aberonni/dotfiles.git"
 
-source ./utils.sh
+ESC_SEQ="\x1b["
+COL_RESET=${ESC_SEQ}"39;49;00m"
+COL_RED=${ESC_SEQ}"31;01m"
+COL_GREEN=${ESC_SEQ}"32;01m"
+COL_YELLOW=${ESC_SEQ}"33;01m"
+COL_BLUE=${ESC_SEQ}"34;01m"
+COL_MAGENTA=${ESC_SEQ}"35;01m"
+COL_CYAN=${ESC_SEQ}"36;01m"
+
+function run(){
+    echo -e "${COL_MAGENTA} ⇒ ${COL_RESET}$1..."
+}
+
+function message(){
+    echo -e "${COL_BLUE}[INFO] - ${COL_RESET}$1..."
+}
+
+function warn(){
+    echo -e "${COL_YELLOW}[WARNING] - ${COL_RESET} "$1
+}
+
+function error(){
+    echo -e "${COL_RED}[ERROR] - ${COL_RESET} "$1
+}
+
+# Test whether a command exists
+# $1 - cmd to test
+function command_exists(){
+    if [ $(type -P $1) ]; then
+      return 0
+    fi
+    return 1
+}
 
 function check_requirements(){
   # If missing, download and extract the dotfiles repository
@@ -166,6 +198,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 check_requirements
 
+source ./utils.sh
 source ./scripts/apps.sh
 source ./scripts/dotfiles.sh
 source ./scripts/fonts.sh
--- utils.sh
+++ utils.sh
@@ -1,28 +1,3 @@
-ESC_SEQ="\x1b["
-COL_RESET=${ESC_SEQ}"39;49;00m"
-COL_RED=${ESC_SEQ}"31;01m"
-COL_GREEN=${ESC_SEQ}"32;01m"
-COL_YELLOW=${ESC_SEQ}"33;01m"
-COL_BLUE=${ESC_SEQ}"34;01m"
-COL_MAGENTA=${ESC_SEQ}"35;01m"
-COL_CYAN=${ESC_SEQ}"36;01m"
-
-function run(){
-    echo -e "${COL_MAGENTA} ⇒ ${COL_RESET}$1..."
-}
-
-function message(){
-    echo -e "${COL_BLUE}[INFO] - ${COL_RESET}$1..."
-}
-
-function warn(){
-    echo -e "${COL_YELLOW}[WARNING] - ${COL_RESET} "$1
-}
-
-function error(){
-    echo -e "${COL_RED}[ERROR] - ${COL_RESET} "$1
-}
-
 function ask(){
     echo -e "${COL_YELLOW} ${COL_RESET}  [?]"$1
     read -r
@@ -30,15 +5,6 @@ function ask(){
 
 function get_answer(){
     printf "%s" "$REPLY"
-}
-
-# Test whether a command exists
-# $1 - cmd to test
-function command_exists(){
-    if [ $(type -P $1) ]; then
-      return 0
-    fi
-    return 1
 }
 
 function link(){
