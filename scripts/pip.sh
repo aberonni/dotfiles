2d51b96ed9211956799ffc3073bc4d21f69ce86e
Domenico Gemoli <domenicogemoli@gmail.com>
2018-01-22 14:29:30 +0100

    separate pip messages

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -3,8 +3,9 @@
 PIP_PACKAGES="${RESOURCES_DIRECTORY}/pip/pip_packages"
 
 function install_pip_packages(){
-  run "install pip packages"
+  run "installing pip"
   sudo easy_install pip
+  run "install pip packages"
   cat ${PIP_PACKAGES} | xargs sudo pip install --ignore-installed six > /dev/null 2>&1
 }
 
