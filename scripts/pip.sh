cbbef978d0c1a3ead003c5f117643c2378e27125
Domenico Gemoli <domenicogemoli@gmail.com>
2018-01-22 15:35:42 +0100

    fix pip permissions

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -5,8 +5,10 @@ PIP_PACKAGES="${RESOURCES_DIRECTORY}/pip/pip_packages"
 function install_pip_packages(){
   run "installing pip"
   sudo easy_install pip
+  run "fixing permissions"
+  sudo chown -R $USER /usr/local/bin/pbr
   run "install pip packages"
-  cat ${PIP_PACKAGES} | xargs sudo pip install --ignore-installed six > /dev/null 2>&1
+  cat ${PIP_PACKAGES} | xargs sudo pip install --ignore-installed six
 }
 
 function backup_pip_packages(){
