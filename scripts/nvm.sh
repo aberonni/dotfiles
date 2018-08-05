1f5f557a6d1438b8f0d5d96683a6a0447861ca9a
Domenico Gemoli <domenicogemoli@gmail.com>
2018-08-05 17:54:23 +0200

    better npm

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+NVM_DIR="$HOME/.nvm"
+
 function install_nvm {
   run "install nvm"
-  export NVM_DIR="$HOME/.nvm" && (
-      git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-      cd "$NVM_DIR"
-      git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-  ) && \. "$NVM_DIR/nvm.sh"
+
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  \. "$NVM_DIR/nvm.sh"
+
+  cd ${DOTFILES_DIRECTORY}
+  run "installing latest node for nvm"
+  nvm install node
 }
