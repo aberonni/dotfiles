ed40a1c0b2cb4d7c209d62c73bf9d3270b2eb907
Domenico Gemoli <domenicogemoli@gmail.com>
2018-12-22 01:42:52 +0100

    fix link_vscode_settings

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VSCODE_BACKUP_FOLDER="${RESOURCES_DIRECTORY}/vscode/backup"
+
 VSCODE_SUPPORT_DIR="${HOME}/Library/Application Support/Code - Insiders/User"
 VSCODE_SUPPORT_SETTINGS="${HOME}/Library/Application Support/Code - Insiders/User/settings.json"
 VSCODE_SUPPORT_KEYBINDINGS="${HOME}/Library/Application Support/Code - Insiders/User/keybindings.json"
@@ -12,6 +14,25 @@ function create_vscode_dir(){
   DIR=$1
   if [ ! -d "$DIR" ]; then
     mkdir -p "${DIR}"
+  fi
+}
+
+function backup_vscode_settings(){
+  bckfld="${CONFIG_FILES_BACKUP_FOLDER}/$(date +%s)"
+  mkdir -p ${bckfld}
+  
+  if [ -L "$VSCODE_SUPPORT_SETTINGS" ]; then
+    # no need to backup if its a link. just unlink it
+    unlink "${VSCODE_SUPPORT_SETTINGS}"
+  else
+    mv "${VSCODE_SUPPORT_SETTINGS}" ${bckfld}
+  fi
+
+  if [ -L "$VSCODE_SUPPORT_KEYBINDINGS" ]; then
+    # no need to backup if its a link. just unlink it
+    unlink "${VSCODE_SUPPORT_KEYBINDINGS}"
+  else
+    mv "${VSCODE_SUPPORT_KEYBINDINGS}" ${bckfld}
   fi
 }
 
@@ -35,7 +56,10 @@ function setup_vscode(){
   run "creating settings directory"
   create_vscode_dir "${VSCODE_SUPPORT_DIR}"
 
-  run "copy settings"
+  run "backup old settings"
+  backup_vscode_settings
+
+  run "link new settings"
   link_vscode_settings
 
   run "install extensions"
