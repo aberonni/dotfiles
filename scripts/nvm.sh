4bf40daeb262c585645f5e3471a21f8592d90be3
Domenico Gemoli <domenicogemoli@gmail.com>
2023-11-17 12:58:49 +0100

    update nvm install script

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -5,7 +5,7 @@ NVM_DIR="$HOME/.nvm"
 function install_nvm {
   run "install nvm"
 
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
   \. "$NVM_DIR/nvm.sh"
